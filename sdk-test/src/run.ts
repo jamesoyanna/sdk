7a538f8bc50afb5e3b6ba2a3f4e915aeff08336b
jamesoyanna <engrjayt200@gmail.com>
2023-04-24 20:20:26 +0100

    Project completed

---
+++ sdk-test/src/run.ts
@@ -0,0 +1,18 @@
+import Typicode from "oyanna-sdk";
+
+const client: Typicode = new Typicode({
+  apiKey: "sdfd",
+});
+
+client.getPosts().then((p) => {
+  console.log(p);
+});
+
+// client.createPost({
+//     title: "test",
+//     body: "test",
+//     userId: 1,
+//   })
+//   .then((p) => {
+//     console.log(`Created new post with id ${p.id}`);
+//   });
